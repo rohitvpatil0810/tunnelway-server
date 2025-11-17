dd8a298ce55c595259295d64dc7aaca088eff326
Rohit Patil <rohitvpatil0810@gmail.com>
2025-11-17 23:11:54 +0530

    feat: implement basic echo websocket server

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/rohitvpatil0810/tunnelway-server
 go 1.25.3
 
 require github.com/go-chi/chi v1.5.5
+
+require github.com/gorilla/websocket v1.5.3 // indirect
--- internal/handlers/hello.go
+++ internal/handlers/hello.go
@@ -1,9 +1,11 @@
 package handlers
 
 import (
-	"encoding/json"
+	"fmt"
+	"log/slog"
 	"net/http"
 
+	"github.com/gorilla/websocket"
 	"github.com/rohitvpatil0810/tunnelway-server/pkg/logger"
 )
 
@@ -11,18 +13,36 @@ type HelloMessage struct {
 	Message string
 }
 
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
 func SayHello(w http.ResponseWriter, r *http.Request) {
 	var log = logger.Logger()
 
-	log.Info("First Api Hit: \"/\" (SayHello)")
-	var helloMessage = HelloMessage{
-		Message: "Hello From Tunnelway Server...",
-	}
-	w.Header().Set("Content-Type", "application/json")
-	err := json.NewEncoder(w).Encode(helloMessage)
+	conn, err := upgrader.Upgrade(w, r, nil)
+
 	if err != nil {
-		log.Error(err.Error())
+		log.Error("Error upgrading to websocket", slog.String("error", err.Error()))
 		return
 	}
+	defer conn.Close()
 
+	// echo back received messages
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Error("Error reading message", slog.String("error", err.Error()))
+			break
+		}
+
+		fmt.Printf("Received: %s\\n", message)
+
+		if err := conn.WriteMessage(websocket.TextMessage, message); err != nil {
+			log.Error("Error writing message:", slog.String("error", err.Error()))
+			break
+		}
+	}
 }
